353873842dea3ebca1ca29e3c992d9716467974f
Kenneth Cho <chokn@users.noreply.github.com>
2025-10-21 20:47:49 -0400

    fix domain assets issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 // Use VITE_BASE when provided (e.g., by CI for GitHub Pages), otherwise default to '/'
-const base = process.env.VITE_BASE || "/";
+const base = "/";
 
 export default defineConfig({
   plugins: [react()],
